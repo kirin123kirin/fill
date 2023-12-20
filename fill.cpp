5cb37eed0eb60ceec60d1d3339a5b7566baadf8f
kirin123 <kirin123kirin@gmail.com>
2023-12-20 23:42:47 +0900

    v0.0.2

--- fill.cpp
+++ fill.cpp
@@ -1,88 +1,78 @@
 #include <windows.h>
 #include "argparser.hpp"
+#include "clipboard.hpp"
 
 const wchar_t delim = L'\t';
 const wchar_t* lineterminator = L"\r\n";
 
+void retwrite(ClipboardText<wchar_t>& dest, const std::vector<std::wstring_view>& src) {
+    for(auto& it : src) {
+        dest.write(it);
+        dest.write(delim);
+    }
+    dest.seek(-1, 1);
+    dest.write(lineterminator, 2);
+}
+
 int wmain(int argc, wchar_t* argv[]) {
     std::wcout.imbue(std::locale("Japanese", std::locale::ctype));
     setlocale(LC_ALL, "");
+    bool skip_row = false;
+    wchar_t *txt = NULL;
+    std::vector<std::wstring_view> pre, cur;
+    std::size_t len = 0;
+    int i = 0;
 
-    if(argc != 1) {
-        std::wcerr << L"引数は指定できません" << std::endl;
+    auto ap = ArgParser(L"Excel表をコピーして、下の行の空白を上の行の値で値保管するプログラム.\n", argc, argv);
+    ap.add(L"-s", L"--skip_row", &skip_row, L"詳細行のみ表示する\n");
+    ap.parse();
+
+    // const wchar_t txt[] = L"111\t\t\r\n\taaa\t\r\n\tbb\tA\r\n222\t\t\r\n\tc\t\r\n\t\tB\r\n";
+    if(readclip(txt) == false)
         return 1;
-    }
+    len = wcslen(txt);
+    ClipboardText<wchar_t> dest('w', len * 2);
 
-    if(OpenClipboard(nullptr) == false) {
-        std::wcerr << L"クリップボードを開けませんでした。" << std::endl;
-        return 1;
-    }
-
-    HANDLE hData = GetClipboardData(CF_UNICODETEXT);
-
-    if(hData != nullptr) {
-        wchar_t* txt = static_cast<wchar_t*>(GlobalLock(hData));
-        const wchar_t *a = txt, *p = txt, *start = txt;
-        std::vector<std::wstring> prev;
-        std::vector<std::wstring> cur;
-
-        if(txt != nullptr) {
-            GlobalUnlock(hData);
-
-            while(*p != L'\r') {
-                if(*p == delim) {
-                    prev.emplace_back(start, p);
-                    start = p + 1;
-                }
-                ++p;
+    for(const wchar_t *p = txt, *start = p; *p; ++p) {
+        if(*p != L'\n')
+            continue;
+        if(i > 1)
+            cur.clear();
+        for(const wchar_t* pp = start; pp != p + 1; ++pp) {
+            if(*pp == delim || *pp == L'\r') {
+                if(i == 0)
+                    pre.emplace_back(start, pp - start);
+                else
+                    cur.emplace_back(start, pp - start);
+                start = pp + 1;
             }
-            prev.emplace_back(start, p++);
-            std::wstring ret(a, ++p);
-            for(start = p; *p; ++p) {
-                if(*p == L'\r') {
-                    cur.emplace_back(start, p++);
-                    for(int i = 0, len = cur.size(); i < len; ++i) {
-                        if(cur[i].empty() && !prev[i].empty()) {
-                            if(i == 0 || cur[i - 1] == prev[i - 1])
-                                cur[i] = prev[i];
-                        }
-                        ret.append(cur[i] + delim);
-                    }
-                    ret.append(lineterminator);
-                    prev = cur;
-                    start = p + 1;
-                    cur.clear();
-                } else if(*p == delim) {
-                    cur.emplace_back(start, p);
-                    start = p + 1;
-                }
-            }
-            GlobalUnlock(hData);
-
-            if(!ret.empty()) {
-                EmptyClipboard();
-                hData = GlobalAlloc(GMEM_MOVEABLE, (ret.size() + 1) * sizeof(wchar_t));
-                if(hData != nullptr) {
-                    wchar_t* pData = static_cast<wchar_t*>(GlobalLock(hData));
-                    wcscpy_s(pData, ret.size() + 1, ret.data());
-                    GlobalUnlock(hData);
-
-                    SetClipboardData(CF_UNICODETEXT, hData);
-                } else {
-                    std::wcerr << L"クリップボードデータのメモリ確保に失敗しました。" << std::endl;
-                }
-
-                if(hData != nullptr)
-                    std::wcerr << L"クリップボードにデータを格納しました。Excelに貼り付けてください" << std::endl;
-            } else {
-                std::wcerr << L"fillに失敗しました" << std::endl;
-            }
-
-        } else {
-            std::wcerr << L"クリップボードのテキストを取得できませんでした。" << std::endl;
         }
+        if(i != 0) {
+            bool writable = true;
+            for(int j = 0, end = cur.size(); j < end; ++j) {
+                if(j > 0 && cur[j - 1] != pre[j - 1])
+                    continue;
+                if(cur[j].empty()) {
+                    if(!pre[j].empty())
+                        cur[j] = pre[j];
+                } else if(skip_row && pre[j].empty()) {
+                    writable = false;
+                }
+            }
+            if(writable)
+                retwrite(dest, pre);
+            pre = cur;
+        }
+        start = p + 1;
+        ++i;
     }
-    CloseClipboard();
+    if(i == 0)
+        return 1;
+    else if(i == 1)
+        dest.write(txt, len);
+    else
+        retwrite(dest, cur);
 
+    dest.close();
     return 0;
 }
